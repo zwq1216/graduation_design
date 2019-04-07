70fe60d11f38b8d8f5b4a3b98f1e311f5d050a45
zwq1216 <2427011918@qq.com>
2019-04-07 19:19:54 +0800

    毕设第一次提交

---
+++ community_management/community_management/local_setting.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
